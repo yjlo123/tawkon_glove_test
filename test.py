fff7ffe3da16ce14efd2c235ea277c5237c82d5d
Siwei <liusiwei.yjlo@gmail.com>
2014-02-09 22:33:51 +0200

    run on multi devices

--- test.py
+++ test.py
@@ -13,25 +13,43 @@ if len(sys.argv) != 2:
 
 country = sys.argv[1]
 
-device = MonkeyRunner.waitForConnection()
-print "[Glove-test] start testing ("+country+")"
-print "[Glove-test] connected device"
-screen_width = int(device.getProperty("display.width"))
-screen_height = int(device.getProperty("display.height"))
-device_id = device.getSystemProperty("build.ID")
-print "[Glove-test] device ID: " + str(device_id)
-print "[Glove-test] screen resolution: " + str(screen_width) + " * " + str(screen_height)
+devices = os.popen('adb devices').read().strip().split('\n')[1:]
 
-# Installs the Android package. Notice that this method returns a boolean, so you can test
-# to see if the installation worked.
-device.installPackage('glove_test/glove'+country+'.apk')
-print "[Glove-test] install package"
+if len(devices) == 0:
+	print "No connected device."
+	sys.exit(0)
+
+print "Devices: "+str(devices)
+
+deviceid = devices[0].split('\t')[0]
+device = MonkeyRunner.waitForConnection(5, deviceid)
+
 package = 'com.glove'
 activity = 'com.glove.activity.RoutingActivity'
 runComponent = package + '/' + activity
 
-device.startActivity(component=runComponent)
-MonkeyRunner.sleep(4)
+screen_width = 0
+screen_height = 0
+
+#init
+def init():
+	global screen_width
+	global screen_height
+
+	screen_width = int(device.getProperty("display.width"))
+	screen_height = int(device.getProperty("display.height"))
+
+	print "[Glove-test] start testing ("+country+")"
+	print "[Glove-test] connected device"
+	print "[Glove-test] device ID: " + str(deviceid)
+	print "[Glove-test] screen resolution: " + str(screen_width) + " * " + str(screen_height)
+
+	# Installs the Android package. Notice that this method returns a boolean, so you can test
+	# to see if the installation worked.
+	device.installPackage('glove_test/glove'+country+'.apk')
+	print "[Glove-test] install package"
+	device.startActivity(component=runComponent)
+	MonkeyRunner.sleep(4)
 
 # === Tutorial
 def tutorial():
@@ -140,16 +158,16 @@ def how():
 	print "[Screenshot] how"
 	MonkeyRunner.sleep(1)
 	device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
-	MonkeyRunner.sleep(0.5)
-	device.touch(screen_width/2, screen_height-10, 'DOWN_AND_UP')
 	MonkeyRunner.sleep(1)
 
 # ===click offer
 def offer():
+	MonkeyRunner.sleep(2)
 	device.touch(screen_width/2, screen_height-10, 'DOWN_AND_UP')
 	print "[Action] click GET AN OFFER"
 	MonkeyRunner.sleep(1)
 	device.type('123123')
+	MonkeyRunner.sleep(3)
 	device.press('KEYCODE_DPAD_DOWN', MonkeyDevice.DOWN_AND_UP)
 	device.press('KEYCODE_DPAD_RIGHT', MonkeyDevice.DOWN_AND_UP)
 	device.press('KEYCODE_ENTER', MonkeyDevice.DOWN_AND_UP)
@@ -186,6 +204,8 @@ def left_btn(name):
 	MonkeyRunner.sleep(1)
 	device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 	MonkeyRunner.sleep(0.5)
+	device.touch(screen_width/2, screen_height-10, 'DOWN_AND_UP')
+	MonkeyRunner.sleep(1)
 
 def right_btn(name):
 	for num in range(1,5):
@@ -205,39 +225,63 @@ def right_btn(name):
 	device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 	MonkeyRunner.sleep(1)
 
-if country == "IL":
-	tutorial()
-	nap_email()
-	nap_num()
-	wait_for_result(30)
-	result(5)
-	swipe_back(2)
-	how()
-	offer()
-	left_btn("info")
-	right_btn("price")
-elif country == "US":
-	tutorial()
-	nap_email()
-	wait_for_result(35)
-	result(4)
-	swipe_back(2)
-	how()
-	right_btn("info")
-else:
-	print "[Glove-test] Invalid country"
+def run():
+	if country == "IL":
+		
+		tutorial()
+		nap_email()
+		nap_num()
+		wait_for_result(30)
+		result(5)
+		swipe_back(2)
+		how()
+		
+		offer()
+		left_btn("info")
+		right_btn("price")
+	elif country == "US":
+		tutorial()
+		nap_email()
+		wait_for_result(60)
+		result(4)
+		swipe_back(2)
+		how()
+		right_btn("info")
+	else:
+		print "[Glove-test] Invalid country"
 
 
 # ===uninstall package
-device.removePackage (package)
-print "[Glove-test] remove package"
+def uninstall():
+	device.removePackage (package)
+	print "[Glove-test] remove package"
 
-dst = screenshot_path+"/"+str(screen_width)+" "+str(screen_height)
-os.makedirs(dst)
+	dst = screenshot_path+"/"+str(screen_width)+" "+str(screen_height)
+	os.makedirs(dst)
 
-for pngfile in glob.iglob(os.path.join(screenshot_path, "*.png")):
-    shutil.move(pngfile, dst)
-data = open(dst+'/data.txt', 'w')
-data.write("country="+country)
-data.closed
-print "[Glove-test] finished"
+	for pngfile in glob.iglob(os.path.join(screenshot_path, "*.png")):
+	    shutil.move(pngfile, dst)
+	data = open(dst+'/data.txt', 'w')
+	data.write("country="+country)
+	data.closed
+	print "[Glove-test] finished"
+
+def main():
+	global deviceid
+	global device
+
+	print "Number of devices: "+str(len(devices))
+	print "Testing device: 0"
+	init()
+	run()
+	uninstall()
+
+	for n in range(1, len(devices)):
+		print "Testing device: "+str(n)
+		deviceid = devices[n].split('\t')[0]
+		device = MonkeyRunner.waitForConnection(5, deviceid)
+		init()
+		run()
+		uninstall()
+
+main()
